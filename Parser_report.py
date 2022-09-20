212153bf06f90cb5d3806f583882626c643829d2
ovchinnikovdanila <57699376+ovchinnikovdanila@users.noreply.github.com>
2022-09-20 16:48:37 +0700

    v0.2

--- Parser_report.py
+++ Parser_report.py
@@ -1,48 +1,126 @@
 import csv
+import re
+
+def init_class_objects(filename):
+	global count_bak
+	global count_mag
+	global count_spec
+	global bak_object
+	global mag_object
+	global spec_object
+	count_bak=0
+	count_mag=0
+	count_spec=0
+	with open(filename,"r", encoding="UTF-8") as f:
+		for row in csv.reader(f, delimiter=";"):
+			if any(field.strip() for field in row):
+				print(row[0])
+				match row[0]:
+					case "Бакалавриат":
+						count_bak += 1
+					case "Магистратура":
+						count_mag += 1
+					case "Специалитет":
+						count_spec += 1
+					case _:
+						print("warring2")
+		bak_object = [Bakalavriat(f"{i}") for i in range(count_bak)]
+		mag_object = [Magistratura(f"{i}") for i in range(count_mag)]
+		spec_object = [Specialitet(f"{i}") for i in range(count_spec)]
+	with open(filename,"r", encoding="UTF-8") as f:
+		cb = 0
+		cm = 0
+		cs = 0
+		for row in csv.reader(f, delimiter=";"):
+			if any(field.strip() for field in row):
+				match row[0]:
+					case "Бакалавриат":
+						print("da2")
+						bak_object[cb].type_of_profile = row[1]
+						bak_object[cb].type_of_direction = row[2]
+						cb += 1
+					case "Магистратура":
+						mag_object[cm].type_of_profile = row[1]
+						mag_object[cm].type_of_direction = row[2]
+						cm += 1
+					case "Специалитет":
+						spec_object[cs].type_of_profile = row[1]
+						spec_object[cs].type_of_direction = row[2]
+						cs += 1
+					case _:
+						print("warring3")
 
 
 def reader(filename):
-		with open(filename, "r") as f:
-			for row in csv.reader(f, delimiter=";"):
-				if any(field.strip() for field in row):
-					numer_of_n = str(check_obr_prog(row[5]))
-					match numer_of_n:
-						case "1":
-							eform1.change_culc(row[8], row[9], row[12], row[13])
-						case "2":
-							eform2.change_culc(row[8], row[9], row[12], row[13])
-						case _:			
-							print("unnoun")
-			create_report_docx()
+	with open(filename, "r", encoding="UTF-8") as f:
+		for row in csv.reader(f, delimiter=";"):
+			if any(field.strip() for field in row):
+				normal_string=""
+				match row[0]:
+					case "Бакалавриат":
+						for item in row:
+							if any(field.strip() for field in item):
+								normal_string += item + "***"
+						bakalavr = use_normal_string(normal_string)
+						bakalavr.bak()
+					case "Магистратура":
+						for item in row:
+							if any(field.strip() for field in item):
+								normal_string += item + "***"
+						magister = use_normal_string(normal_string)
+						magister.mag()
+					case "Специалитет":
+						for item in row:
+							if any(field.strip() for field in item):
+								normal_string += item + "***"
+						specialist = use_normal_string(normal_string)
+						specialist.spec()
+					case _:
+						print("warring1")
 
+def output():
+	for i in range(count_bak):
+		print(bak_object[i].type_id + " " + bak_object[i].type_of_profile + " " + bak_object[i].type_of_direction + " среднее по 1 вопросу: " + str(bak_object[i].avgqwn1))
+	for i in range(count_mag):
+		print(mag_object[i].type_id + " " + mag_object[i].type_of_profile + " " + mag_object[i].type_of_direction + " среднее по 1 вопросу: " + str(mag_object[i].avgqwn1))
 
-def check_obr_prog(group):
-	match group:
-		case "404" | "400":
-			return 1
-		case "500" | "600":
-			return 2
-		case _:
-			return 3
+class use_normal_string:
+	def __init__(self, text):
+		self.m = text.split("***")
 
-def create_report_docx():
-	print(str(eform1.avgqwn1))
-	print(str(eform2.avgqwn2))
+	def bak(self):
+		for i in range(count_bak):
+			if self.m[1] == bak_object[i].type_of_profile and self.m[2] == bak_object[i].type_of_direction:
+				print("da")
+				bak_object[i].change_culc(self.m[3],self.m[4],self.m[5],self.m[6])  #ИНДЕКСЫ М ПО ПОРЯДКУ ВОПРОСОВ
+	def mag(self):
+		for i in range(count_mag):
+			if self.m[1] == mag_object[i].type_of_profile and self.m[2] == mag_object[i].type_of_direction:
+				mag_object[i].change_culc(self.m[3],self.m[4],self.m[5],self.m[6])
+	def spec(self):
+		for i in range(count_spec):
+			if self.m[1] == spec_object[i].type_of_profile and self.m[2] == spec_object[i].type_of_direction:
+				spec_object[i].change_culc(self.m[3],self.m[4],self.m[5],self.m[6])
 
-class Obr_progs():
-	counter = 0
-	qwn1 = 0
-	qwn2 = 0
-	qwn3 = 0
-	qwn4 = 0
-	avgqwn1 = 0
-	avgqwn2 = 0
-	avgqwn3 = 0
-	avgqwn4 = 0
-	avgqwn1_2 = 0
-	avgqwn3_4 = 0
+class Bakalavriat:
+	def __init__(self, name_id):
+		self.name_id = name_id
+		self.type_id = "Бакалавриат"
+		self.type_of_profile = ""
+		self.type_of_direction = ""
+		self.counter = 0
+		self.qwn1 = 0					#Тут индекы вопросов
+		self.qwn2 = 0
+		self.qwn3 = 0
+		self.qwn4 = 0
+		self.avgqwn1 = 0				#ТУТ СРЕДНИЕ МЕЖДУ КАКИМИ
+		self.avgqwn2 = 0
+		self.avgqwn3 = 0
+		self.avgqwn4 = 0
+		self.avgqwn1_2 = 0
+		self.avgqwn3_4 = 0
 
-	def change_culc(self, q1, q2, q3, q4):
+	def change_culc(self, q1, q2, q3, q4):    #Добавить количествог вопросов ДОПИСАТЬ КУШКИ
 		self.counter += 1
 		self.qwn1 += int(q1)
 		self.qwn2 += int(q2)
@@ -55,7 +133,42 @@ class Obr_progs():
 		self.avgqwn1_2 = self.avgqwn1 + self.avgqwn2
 		self.avgqwn3_4 = self.avgqwn3 + self.avgqwn4
 
+class Magistratura(Bakalavriat):
+	def __init__(self, name_id):
+		self.name_id = name_id
+		self.type_id = "Магистратура"
+		self.type_of_profile = ""
+		self.type_of_direction = ""
+		self.counter = 0
+		self.qwn1 = 0
+		self.qwn2 = 0
+		self.qwn3 = 0
+		self.qwn4 = 0
+		self.avgqwn1 = 0
+		self.avgqwn2 = 0
+		self.avgqwn3 = 0
+		self.avgqwn4 = 0
+		self.avgqwn1_2 = 0
+		self.avgqwn3_4 = 0
+		
+class Specialitet(Bakalavriat):
+	def __init__(self, name_id):
+		self.name_id = name_id
+		self.type_id = "Специалитет"
+		self.type_of_profile = ""
+		self.type_of_direction = ""
+		self.counter = 0
+		self.qwn1 = 0
+		self.qwn2 = 0
+		self.qwn3 = 0
+		self.qwn4 = 0
+		self.avgqwn1 = 0
+		self.avgqwn2 = 0
+		self.avgqwn3 = 0
+		self.avgqwn4 = 0
+		self.avgqwn1_2 = 0
+		self.avgqwn3_4 = 0
 
-eform1 = Obr_progs()
-eform2 = Obr_progs()
+init_class_objects("n_list.csv")
 reader("input.csv")
+output()
